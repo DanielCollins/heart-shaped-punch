5f8bb589ec82bfdbdae33043bce6a19d674e63e2
Daniel Collins <mail@dcollins.info>
2013-06-09 05:21:03 +1000

    first attempt at a working handshake

--- hsp.c
+++ hsp.c
@@ -15,41 +15,90 @@
 
 #define PORT "42000"
 
+#define CON 0
+#define ACK 1
+#define EST 2
+
+enum HostState
+{
+  DEAD,
+  CONNECTING,
+  ACKNOWLEDGING,
+  ESTABLISHED
+};
+
 int sock;
 struct sockaddr_in out_addr, in_addr;
-socklen_t out_addr_len, in_addr_len;
+struct sockaddr client_addr;
+socklen_t out_addr_len, in_addr_len, client_addr_len;
+enum HostState state;
+
+void acknowledge(void)
+{
+  char b = ACK;
+  sendto(sock, &b, 1, 0, &client_addr, client_addr_len);
+}
+
+void establish(void)
+{ 
+  char b = EST;
+  sendto(sock, &b, 1, 0, &client_addr, client_addr_len);      
+}
+
+void _connect(void)
+{ 
+  char b = CON;
+  sendto(sock, &b, 1, 0, &client_addr, client_addr_len);      
+}
 
 void udp_readable_cb(EV_P_ ev_io *w, int revents)
 {
-  socklen_t bytes_read;
-  char buffer[1001];
+  char b;
   (void) loop;
   (void) w;
   (void) revents;
-  bytes_read = recvfrom(sock, buffer, 1000, 0, (struct sockaddr*) &out_addr,         
-   (socklen_t*) &out_addr_len);
-  buffer[bytes_read] = '\0';
-  printf(": %s\n", buffer);
+  recvfrom(sock, &b, 1, 0, &client_addr, &client_addr_len);
+  switch (b)
+  {
+    case CON:
+      if (state == DEAD || state == CONNECTING)
+      {
+        acknowledge();
+        state = ACKNOWLEDGING;
+      }  
+      return;
+    case ACK:
+      if (state == CONNECTING || state == ESTABLISHED)
+      {
+        establish();
+        state = ESTABLISHED;
+        printf("established");
+      }
+      return;
+    case EST:
+      printf("established\n");
+      state = ESTABLISHED;
+      return;
+  }
+  fprintf(stderr, "warning: bad packet\n");
 }
 
-void spam(char *host)
+void lookup(char *host)
 {
   struct addrinfo hints, *servinfo;
   int rv;
-  char buffer[] = "spam";
-
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_DGRAM;
   if ((rv = getaddrinfo(host, PORT, &hints, &servinfo)) != 0)
   {
-    fprintf(stderr, "gettaddrifo: %s\n", gai_strerror(rv));
+    fprintf(stderr, "gettaddrinfo: %s\n", gai_strerror(rv));
     return;
   }
   if (!servinfo) return;
-  sendto(sock, buffer, strlen(buffer), 0, servinfo->ai_addr,
-           servinfo->ai_addrlen);
-  freeaddrinfo(servinfo);
+  memcpy(&client_addr, servinfo->ai_addr, servinfo->ai_addrlen);
+  client_addr_len = servinfo->ai_addrlen;
+/*  freeaddrinfo(servinfo); */
 }
 
 int main(int argc, char *argv[])
@@ -57,8 +106,10 @@ int main(int argc, char *argv[])
   ev_io udp_watcher;
   struct ev_loop *loop;
 
+  state = DEAD;
   in_addr_len = sizeof(in_addr);
   out_addr_len = sizeof(out_addr);
+  client_addr_len = sizeof(client_addr);
   sock = socket(PF_INET, SOCK_DGRAM, 0);
   fcntl(sock, F_SETFL, O_NONBLOCK);
   bzero(&in_addr, in_addr_len);
@@ -68,8 +119,14 @@ int main(int argc, char *argv[])
   if (bind(sock, (struct sockaddr*) &in_addr, in_addr_len) != 0)
     fprintf(stderr, "could not bind\n");
 
+  state = DEAD;
+
   if (argc == 2)
-    spam(argv[1]);
+  {
+    lookup(argv[1]);
+    state = CONNECTING;
+    _connect();
+  }
 
   loop = ev_default_loop(0);
   ev_io_init(&udp_watcher, udp_readable_cb, sock, EV_READ);
